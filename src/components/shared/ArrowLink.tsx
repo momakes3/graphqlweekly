2a2a8856824dec97d6b8db12eddf909a339a8934
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-12-08 19:51:55 +0330

    Add ArrowLink component

---
+++ src/components/shared/ArrowLink.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import styled from '../style/styled'
+import { Arrow } from '../vectors/Arrow'
+
+interface props {
+  text: string
+  href: string
+  arrow: boolean
+}
+
+export const ArrowLink = ({ text, href, arrow = true }: props) => {
+  return (
+    <LinkWrapper href={href}>
+      <>
+        <LinkText>{text}</LinkText>
+        <Arrow />
+      </>
+    </LinkWrapper>
+  )
+}
+
+const LinkWrapper = styled.a``
+
+const LinkText = styled.span`
+  margin-right: 12px;
+
+  font-family: 'Rubik';
+  font-weight: 500;
+  line-height: 18px;
+  font-size: 18px;
+  text-align: right;
+  color: #ffffff;
+`

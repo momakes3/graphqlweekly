8d70c3b91fae918862c5664106422a3dc91d470c
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-12-08 19:49:00 +0330

    Add 2 line components

---
+++ src/components/shared/Input/HLine.tsx
@@ -0,0 +1,8 @@
+import styled from '../../style/styled'
+
+export const HLine = styled.div`
+  width: 100%;
+  height: 1px;
+  margin: 40px 0;
+  background: #dadbe3;
+`
---
+++ src/components/shared/Input/VLine.tsx
@@ -0,0 +1,8 @@
+import styled from '../../style/styled'
+
+export const VLine = styled.div`
+  width: 1px;
+  height: 40px;
+  margin: 0 24px;
+  background: #dadbe3;
+`

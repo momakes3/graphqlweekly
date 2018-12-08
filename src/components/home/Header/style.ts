53a6703aa8a180738e9f8b32ac620ac7a4f310ca
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-12-08 17:34:55 +0330

    Add Header styles

--- src/components/home/Header/style.ts
+++ src/components/home/Header/style.ts
@@ -1,7 +1,8 @@
 import styled, { css } from '../../style/styled'
 import { mobile, desktop } from '../../style/media'
 
-let iconsWidth = 45
+// images
+import bgSvg from './bg.svg'
 
 export const Grid = styled.div`
   display: flex;
@@ -12,111 +13,25 @@ export const Grid = styled.div`
   `)};
 `
 
-export const ContentWrapper = styled.div`
-  ${desktop(css`
-    margin-right: 60px;
-  `)};
-  ${mobile(css`
-    margin-bottom: 40px;
-  `)};
+export const HeaderWrapper = styled.header`
+  background-image: url(${bgSvg});
+  background-size: cover;
+  background-position: top center;
+  height: 595px;
 `
 
-export const Title = styled.h1`
-  line-height: 1;
-  font-weight: 700;
-  margin: 0;
+export const PageTitle = styled.h1`
+  margin: 0 auto;
+  max-width: 667px;
 
-  ${desktop(css`
-    font-size: ${p => p.theme.fontHuge48}px;
-  `)};
+  font-weight: 400;
+  line-height: 1.1;
+  font-size: 44px;
+  text-align: center;
 
-  ${mobile(css`
-    font-size: ${p => p.theme.fontLarge32}px;
-  `)};
-`
+  color: #ffffff;
 
-// -
-export const Highlights = styled.div``
-
-export const Highlight = styled.div`
-  display: flex;
-  align-items: flex-start;
-`
-
-export const Icon = styled.div`
-  width: ${iconsWidth}px;
-  height: 55px;
-  display: inline-flex;
-  align-items: center;
-
-  svg * {
-    storke: ${p => p.theme.primaryLight1};
+  strong {
+    font-weight: 500;
   }
-`
-
-export const HighlightText = styled.div<{ noBorder?: boolean }>`
-  width: 100%;
-  max-width: 430px;
-  padding-top: 16px;
-  padding-bottom: 16px;
-
-  line-height: 1.2;
-  font-size: ${p => p.theme.fontMedium20}px;
-  font-weight: 600;
-
-  border-bottom: 1px solid ${p => p.theme.primaryLight2};
-  color: ${p => p.theme.primary};
-
-  ${p =>
-    p.noBorder &&
-    css`
-      border: none;
-    `};
-
-  ${mobile(css`
-    font-size: ${p => p.theme.fontBase}px;
-  `)};
-`
-
-export const Buttons = styled.div`
-  display: inline-flex;
-`
-
-// -
-export const Github = styled.a`
-  display: inline-flex;
-  color: ${p => p.theme.primary};
-`
-
-export const GithubIcon = styled.div`
-  width: ${iconsWidth}px;
-  margin-top: -2px;
-  svg {
-    stroke: ${p => p.theme.primaryLight1};
-  }
-`
-
-// -
-export const CodeWrapper = styled.div`
-  flex-grow: 0;
-  flex-shrink: 1;
-  width: auto;
-  flex-basis: 580px;
-  max-width: 580px;
-  height: 580px;
-  margin-left: auto;
-  position: relative;
-  margin-bottom: -32px;
-  border-radius: ${p => p.theme.radiusLarge}px;
-
-  ${mobile(css`
-    margin-left: -16px;
-    margin-right: -16px;
-    height: 600px;
-    flex-basis: auto;
-    width: calc(100% + 32px);
-    max-width: 490px;
-
-    margin-bottom: 32px;
-  `)};
 `

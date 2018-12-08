ec0183065e32594a1a451fce6e622dcd37656f35
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-12-08 19:50:21 +0330

    Add Primary button

---
+++ src/components/shared/Buttons/PrimaryButton.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import styled from '../../style/styled'
+
+interface Props {
+  text: String
+  icon?: any
+}
+
+export const PrimaryButton = ({ text, icon }: Props) => (
+  <Wrapper>
+    {icon && <IconWrapper>{icon}</IconWrapper>}
+    <Text>{text}</Text>
+  </Wrapper>
+)
+
+const Wrapper = styled.button`
+  padding: 12px 17px;
+  background: ${p => p.theme.purple};
+  box-shadow: 0px 4px 10px rgba(23, 43, 58, 0.25);
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+
+  border: none;
+  outline: none;
+`
+
+const Text = styled.span`
+  font-weight: 500;
+  line-height: 1;
+  font-size: ${p => p.theme.fontBase}px;
+  text-transform: uppercase;
+
+  color: ${p => p.theme.white};
+`
+
+const IconWrapper = styled.div`
+  margin-right: 12px;
+`

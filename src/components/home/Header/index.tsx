b45936eb554aeea5cf02df152e7a6804664bc37d
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-12-08 17:34:34 +0330

    Add Header components

--- src/components/home/Header/index.tsx
+++ src/components/home/Header/index.tsx
@@ -4,15 +4,26 @@ import * as React from 'react'
 import { Nav } from '../../shared/Nav'
 import { Space } from '../../shared/Space'
 import { Container } from '../../shared/Container'
+import { HeaderWrapper, PageTitle } from './style'
+import { Subscription } from '../Subscription'
 
 export const Header = () => {
   return (
-    <header style={{ background: 'white' }}>
+    <HeaderWrapper>
       <Nav />
 
-      <Space height={100} heightOnMobile={64} />
+      <Space height={80} heightOnMobile={32} />
 
-      <Container />
-    </header>
+      <Container>
+        <PageTitle>
+          A weekly newsletter of the best <strong>news</strong>,{' '}
+          <strong>articles</strong> and <strong>projects</strong> about GraphQL
+        </PageTitle>
+
+        <Space height={56} heightOnMobile={32} />
+
+        <Subscription />
+      </Container>
+    </HeaderWrapper>
   )
 }

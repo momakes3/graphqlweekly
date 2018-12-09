e0ba6a0c23299cb57fb058351303b4285290fe65
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-12-10 00:07:23 +0330

    Add Side bar Line

---
+++ src/components/home/Content/SidebarLine.tsx
@@ -0,0 +1,10 @@
+import styled from '../../style/styled'
+
+export const SidebarLine = styled.div`
+  width: auto;
+  height: 1px;
+  background: #9da0b5;
+  opacity: 0.5;
+
+  margin: 48px 0 48px 23px;
+`

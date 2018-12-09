194af5ef759a2cb7923e6b35f61e0cd11aaa7a6a
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-12-10 00:12:13 +0330

    Add Footer to page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { Layout } from '../components/shared/Layout'
 import { Header } from '../components/home/Header'
 import { MetaTags } from '../components/shared/MetaTags'
 import { Content } from '../components/home/Content'
+import { Footer } from '../components/home/Footer'
 
 export default () => (
   <Layout>
@@ -14,5 +15,6 @@ export default () => (
 
     <Header />
     <Content />
+    <Footer />
   </Layout>
 )

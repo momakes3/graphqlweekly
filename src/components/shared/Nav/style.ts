754dbf3ef23dc8a693d8c86d7610bb618e7dc289
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-12-10 00:13:06 +0330

    Fix responsive problems

--- src/components/shared/Nav/style.ts
+++ src/components/shared/Nav/style.ts
@@ -1,40 +1,80 @@
 // Utils
-import styled from '../../style/styled'
+import styled, { css } from '../../style/styled'
 
 // Local
 import { Link } from '../Link'
+import { mobile } from '../../style/media'
 
 export const Wrapper = styled.nav`
   display: flex;
   min-height: 52px;
-  align-items: center;
   justify-content: space-between;
+
+  ${mobile(css`
+    flex-wrap: wrap;
+    padding-right: 24px;
+    padding-left: 24px;
+  `)};
 `
 
 export const LogoWrapper = styled(Link)`
   display: inline-flex;
   align-items: center;
+  ${mobile(css`
+    /* Hieght & shadow together */
+    svg {
+      width: auto;
+      height: 56px;
+    }
+  `)};
 `
 
-export const NavItems = styled.div`
+export const NavItems = styled.div<{ isOpened: boolean }>`
   display: flex;
   justify-content: flex-end;
   align-items: center;
 
   height: 52px;
+  overflow: hidden;
+
+  ${mobile(css`
+    width: 100%;
+    height: auto;
+    padding-top: 28px;
+
+    flex-direction: column;
+    align-items: center;
+  `)};
+
+  ${p =>
+    p.isOpened &&
+    mobile(css`
+      height: 0;
+    `)};
 `
 
 export const NavItem = styled.a`
   margin-left: 40px;
+
+  ${mobile(css`
+    margin-left: 0;
+    padding: 12px 0;
+  `)};
 `
 
 export const NavText = styled.span`
   margin-right: 12px;
 
-  font-family: 'Rubik';
   font-weight: 500;
-  line-height: 18px;
+  line-height: 1;
   font-size: 18px;
-  text-align: right;
   color: #ffffff;
+`
+
+export const MenuButton = styled.span`
+  width: auto;
+  height: 20px;
+
+  margin-top: 11px;
+  margin-right: 6px;
 `

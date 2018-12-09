2cecdbd54178987b163f508cf3b458ef8240f07d
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-12-10 00:13:22 +0330

    Add menu responsive

--- src/components/shared/Nav/index.tsx
+++ src/components/shared/Nav/index.tsx
@@ -2,17 +2,31 @@ import * as React from 'react'
 
 // Local
 import { Container } from '../Container'
-import { Wrapper, LogoWrapper, NavItems, NavItem, NavText } from './style'
+import {
+  Wrapper,
+  LogoWrapper,
+  NavItems,
+  NavItem,
+  NavText,
+  MenuButton,
+} from './style'
 import LogoSvg from './Logo'
 import { Space } from '../Space'
 import { Arrow } from '../../vectors/Arrow'
+import Button from './Button'
+import Close from './Close'
 
 type Props = {}
-type State = { hovered: string | null; clicked: string | null }
+type State = { isOpened: boolean }
 
 export class Nav extends React.Component<Props, State> {
-  state = { hovered: null, clicked: null }
+  state = { isOpened: true }
 
+  menuClickHandler = () => {
+    this.setState({
+      isOpened: !this.state.isOpened,
+    })
+  }
   render() {
     return (
       <Container>
@@ -22,7 +36,11 @@ export class Nav extends React.Component<Props, State> {
             <LogoSvg />
           </LogoWrapper>
 
-          <NavItems>
+          <MenuButton>
+            {this.state.isOpened ? <Button /> : <Close />}
+          </MenuButton>
+
+          <NavItems isOpened={this.state.isOpened}>
             <NavItem href="#">
               <NavText>What is GraphQL?</NavText>
               <Arrow />

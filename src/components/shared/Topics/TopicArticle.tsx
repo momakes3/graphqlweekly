1427c5dbc3c4da189938b4840a5cb0e73bd552b9
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-12-10 00:12:54 +0330

    Add Special part

--- src/components/shared/Topics/TopicArticle.tsx
+++ src/components/shared/Topics/TopicArticle.tsx
@@ -3,14 +3,21 @@ import styled from 'styled-components'
 
 interface Props {
   title: string
-  text: string
+  text: React.ReactNode
+  special?: string
 }
 
-export const TopicArticle = ({ title, text }: Props) => {
+export const TopicArticle = ({ title, text, special }: Props) => {
   return (
     <>
       <Title>{title}</Title>
       <Text>{text}</Text>
+      {special && (
+        <Text>
+          <strong>Special perk:</strong>
+          {special}
+        </Text>
+      )}
     </>
   )
 }
@@ -25,9 +32,17 @@ const Title = styled.h2`
 `
 
 const Text = styled.p`
-  font-weight: normal;
+  font-weight: 400;
   line-height: 1.75;
   font-size: 16px;
 
   color: #081146;
+
+  strong {
+    font-weight: 500;
+
+    ::after {
+      content: ' ';
+    }
+  }
 `

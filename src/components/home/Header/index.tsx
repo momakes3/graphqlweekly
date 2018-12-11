83aa9e1ec061f76f7c03536b9f5fb9842f44a35e
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-12-11 10:49:01 +0330

    Fix: some styles

--- src/components/home/Header/index.tsx
+++ src/components/home/Header/index.tsx
@@ -4,15 +4,17 @@ import * as React from 'react'
 import { Nav } from '../../shared/Nav'
 import { Space } from '../../shared/Space'
 import { Container } from '../../shared/Container'
-import { HeaderWrapper, PageTitle } from './style'
+import { HeaderWrapper, PageTitle, Socials, SocialItem } from './style'
 import { Subscription } from '../Subscription'
+import Slack from '../../vectors/Slack'
+import Twitter from '../../vectors/Twitter'
 
 export const Header = () => {
   return (
     <HeaderWrapper>
       <Nav />
 
-      <Space height={80} heightOnMobile={32} />
+      <Space height={80} heightOnMobile={27} />
 
       <Container>
         <PageTitle>
@@ -23,6 +25,17 @@ export const Header = () => {
         <Space height={56} heightOnMobile={32} />
 
         <Subscription />
+
+        <Socials>
+          <SocialItem href="#">
+            <Twitter />
+            Twitter
+          </SocialItem>
+          <SocialItem href="#">
+            <Slack stroke="#ffffff" />
+            Slack
+          </SocialItem>
+        </Socials>
       </Container>
     </HeaderWrapper>
   )

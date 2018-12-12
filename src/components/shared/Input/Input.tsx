53c1b33478966d5eadfffeb9f62609731d590b4f
Behnam Rajabifard <b.rajabifard@gmail.com>
2018-12-12 23:16:21 +0330

    Some props added

--- src/components/shared/Input/Input.tsx
+++ src/components/shared/Input/Input.tsx
@@ -5,12 +5,29 @@ import { mobile } from '../../style/media'
 interface Props {
   label: string
   placeholder: string
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  value?: string
+  name?: string
 }
 
-export const Input = ({ label, placeholder }: Props) => (
+export const Input = ({
+  label,
+  placeholder,
+  onChange,
+  value,
+  name,
+  ...props
+}: Props) => (
   <Wrapper>
     <Label>{label}</Label>
-    <InputTag type="text" placeholder={placeholder} />
+    <InputTag
+      type="text"
+      placeholder={placeholder}
+      onChange={onChange}
+      value={value}
+      name={name}
+      {...props}
+    />
   </Wrapper>
 )
 
